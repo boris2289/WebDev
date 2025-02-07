fe0fb92f562502c0200cded5a054b5c33e7c2dd6
boris2289 <bdubovoj05@gmail.com>
2025-02-07 22:01:57 +0500

    lab3

---
+++ LAB3/JSCOURSEFOLDER/VARIABLES.js
@@ -0,0 +1,17 @@
+/* let admin, name;
+name = "John";
+admin = name;
+alert( admin );
+//
+let ourPlanetName = "ZHer";
+let currentUserName = "John";*/
+//
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+
+const AGE = someCode(BIRTHDAY); // make age uppercase?
+console.log(AGE)
+//
+let name = "Quanysh";
+alert( `hello ${1}` );
+alert( `hello ${"name"}` ); 
+alert( `hello ${name}` );
